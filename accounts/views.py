10638ac9599f970562c54be8374c7c4fc8abc627
Brian Omondi <brianayieke@gmail.com>
2025-04-26 20:02:04 +0300

    Change in navigation flow

--- accounts/views.py
+++ accounts/views.py
@@ -2,21 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
-from django.contrib.auth.models import User
 from django.contrib import messages
-
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! You can now log in.')
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/register.html', {'form': form})
 
 def user_login(request):
     if request.method == 'POST':
@@ -27,16 +13,16 @@ def user_login(request):
         if user is not None:
             login(request, user)
             messages.success(request, f'Welcome back, {username}!')
-            return redirect('home')
+            return redirect('home')  # This now goes to landing page
         else:
             messages.error(request, 'Invalid username or password.')
     
     return render(request, 'accounts/login.html')
 
-
 @login_required
 def home(request):
-    return render(request, "home.html")
+    """Landing page view after successful login"""
+    return render(request, "home.html")  # This is our landing page
 
 @login_required
 def diagnosis_home(request):
