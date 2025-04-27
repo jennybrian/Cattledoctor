50b6fa259e9cab18aef3296890e1535d4fced06c
Brian Omondi <brianayieke@gmail.com>
2025-04-27 14:31:11 +0300

    Fixing navigation issues

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import register, user_login, user_logout
+from .views import register, user_login, user_logout, home, diagnosis_home
 
 urlpatterns = [
     path("register/", register, name="register"),
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
+    path("home/", home, name="home"),  # <-- Add this
+    path("diagnosis/", diagnosis_home, name="diagnosis_home"),  # <-- Add this
 ]
