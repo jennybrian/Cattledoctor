c84c7d28446407d1f951feb6eb241fc3cb6caca4
Brian Omondi <brianayieke@gmail.com>
2025-05-15 21:03:47 +0300

    Implement user history 4

--- diagnosis/urls.py
+++ diagnosis/urls.py
@@ -4,5 +4,5 @@ from .views import predict_disease, diagnosis_home
 urlpatterns = [
     path('diagnosis/', diagnosis_home, name='diagnosis_home'),
     path('predict/', predict_disease, name='predict_disease'),
-    path('diagnosis-history/', views.diagnosis_history, name='diagnosis_history'),
+    path('diagnosis-history/', diagnosis_history, name='diagnosis_history'),
 ]
