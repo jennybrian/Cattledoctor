efffdf9f13f14c08b3fb7b20dfdea141e11db710
Brian Omondi <brianayieke@gmail.com>
2025-05-20 01:49:32 +0300

    Fix contourpy version and secure email settings

--- cattle_diagnosis/settings.py
+++ cattle_diagnosis/settings.py
@@ -142,9 +142,9 @@ if not DEBUG:
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # Or your email provider
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'omondibrian231@gmail.com'
-EMAIL_HOST_PASSWORD = 'A2373688'
-DEFAULT_FROM_EMAIL = 'Cattle Diagnosis <omondibrian231@gmail.com>'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
