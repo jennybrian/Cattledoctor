26d74b5cdf5234fc3b328acba9c8d59b29195f4f
Brian Omondi <brianayieke@gmail.com>
2025-05-20 17:53:34 +0300

    Fix: Update URL configuration for predict endpoint (rollback1)

--- cattle_diagnosis/urls.py
+++ cattle_diagnosis/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from diagnosis.views import diagnosis_home, predict_disease
 from accounts import views
 
 urlpatterns = [
@@ -8,4 +9,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/register/', views.register, name='register'),
     path('diagnosis/', include('diagnosis.urls')),
+    path("diagnosis/", diagnosis_home, name="diagnosis_home"),
+    path("predict/", predict_disease, name="predict"),
 ]
